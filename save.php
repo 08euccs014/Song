ea551e25708df762218ddc027cfebd1a0c538419
Mohit Agrawal <mohit@readybytes.in>
2014-02-11 10:26:53 +0530

    Saving logic change

--- save.php
+++ save.php
@@ -1,9 +1,27 @@
 <?php
-	$savedata = $_POST['savedata'];
-	$saveid   = $_POST['saveid'];
+	$savedata 	= getVar('savedata','');
+	$saveToFile = getVar('saveToFile',"/README.txt");
+	$saveid		= getVar('saveid',"");
+	$saveAppend	= getVar('saveAppend',false);
 	
-
-	$res = file_put_contents(dirname(__FILE__)."/".$saveid."/README.txt", $savedata);
+	if($saveAppend)
+		$res = file_put_contents(dirname(__FILE__)."/".$saveid.$saveToFile, $savedata,FILE_APPEND);
+	else
+		$res = file_put_contents(dirname(__FILE__)."/".$saveid.$saveToFile, $savedata);
 	
 	echo $res;
+
+	function getVar($var, $default = null)
+	{
+		if(isset($_POST[$var])) {
+			return $_POST[$var];
+		}
+
+		if(isset($_GEt[$var])) {
+			return $_GET[$var];
+		}
+		
+		return $default;
+	}
+
 ?>
