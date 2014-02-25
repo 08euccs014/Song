ac893e5622f916e42bc4d072d39d664dd73896ff
Mohit Agrawal <mohit@readybytes.in>
2014-02-25 19:22:29 +0530

    Social Manager Description : Administrator Base is added

---
+++ admin/socialman.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @package     Social-Manager.Administrator
+ * @subpackage  com_socialman
+ * @author 		Mohit Agrawal
+ * @since 		Social-Manager 1.0
+ * @copyright   Copyright (C) 2014 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('_JEXEC') or die('Unauthorized Access');
+
+echo "helo";
